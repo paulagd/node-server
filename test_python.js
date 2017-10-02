9749b41fb84b1178ba71bb729ddccc23e0ee3bdf
Paula Gomez Duran <paulagomezduran@MacBook-Pro-de-Paula.local>
2017-10-02 12:43:19 +0100

    established comunication   server -  python  ( WITHOUT API)

--- test_python.js
+++ test_python.js
@@ -1,7 +1,8 @@
 var spawn = require('child_process').spawn,
     py    = spawn('python', ['test_without_api.py']),
-    data = [1,2,3,4,5,6,7,8,9],
-    dataString = '';
+    dataString = '',
+    jsonfile = require('jsonfile');
+
 
 exports.execSentinela = function(req, res, next) {
     // Here we are saying that every time our node application receives data from the
@@ -16,9 +17,21 @@ exports.execSentinela = function(req, res, next) {
     //when it ends...
     py.stdout.on('end', function(){
       console.log('rank_data : ',dataString);
+      var write_this = dataString.trim();
+      var file_to_write_in = './rankins/'+ id ;
+
+      //si no existe el file_to_write_in lo crea. Si existe le añade lo que pongamos.Sin el flag a lo REESCRIBE
+      jsonfile.writeFile(file_to_write_in, write_this, {flag: 'a'}, function (err) {  //flag a is to write in an existing file
+          if(err)
+          console.log(err);
+
+          console.log("DONE");
+      })
+
     });
 
     // FIRST WRITE DATA X CONSOLE...
     py.stdin.write(JSON.stringify(id));
     py.stdin.end();
+
 }
