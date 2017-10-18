f17c3ffc4e5f0b25bdd7ba2671a7e1a184cd78be
Paula Gomez Duran <paulagomezduran@paulagomezduran.local>
2017-10-18 12:35:12 +0100

    added 'encoded image' to postserver function.
    changed port 4242 for 4243

--- controllers/rankins.js
+++ controllers/rankins.js
@@ -1,25 +1,20 @@
 const zerorpc = require("zerorpc");
-
 //allow to get a rankin of an image from the client side
 exports.getRankinById = function (req, res, next) {
     let id = req.params.id;  //id = aaa.json  --> si no ve amb .json al final es una url
-    let { dataset, url } = req.body;
+    let { dataset, url, encoded_image } = req.body;
 
-    if(url){
-      console.log("si que hay URL ___: ",url);
-    }else{
-      console.log("no hay URL, ",url);
-    }
-    getSingleRankin(id, url, dataset, function(data){
+    getSingleRankin(id, url, encoded_image, dataset, function(data){
         res.json(data);
     });
+    // res.json([]); // CASA
 };
 
-getSingleRankin = function(id, url, dataset, callback){
+getSingleRankin = function(id, url, encoded_image, dataset, callback){
 
     let client = new zerorpc.Client();
-    client.connect("tcp://136.206.26.127:4242");
-    client.invoke("postServer", id, url, dataset, function(error, res, more) {
+    client.connect("tcp://136.206.26.127:4243");
+    client.invoke("postServer", id, url, encoded_image, dataset, function(error, res, more) {
         if(error) {
             console.log("ERROR IN CALLBACK");
             callback(error);
